5c9ed3b557bc29fac393672641e6215e4ab20d04
Moussa DIALLO <diallomous@gmail.com>
2023-10-14 16:35:10 +0000

    adding predict

--- main.py
+++ main.py
@@ -1,34 +1,20 @@
-from fastapi import FastAPI, File, UploadFile
-from pydantic import BaseModel
+#from fastapi import FastAPI, File, UploadFile
+#from pydantic import BaseModel
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-from faster_whisper import WhisperModel
-from pydub import AudioSegment
-from io import BytesIO
-import soundfile as sf
+#from faster_whisper import WhisperModel
+#from pydub import AudioSegment
+#from io import BytesIO
+#import soundfile as sf
 # Load the tokenizer and model
 # Load model directly
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-
-tokenizer = AutoTokenizer.from_pretrained("Peed911/french_sentiment_analysis")
-model = AutoModelForSequenceClassification.from_pretrained("Peed911/french_sentiment_analysis")
-label_mapping = {
-    "Negative": 0,
-    "Positive": 1
-}
-labels_name = [ "Negative", "Positive"]
-
+from faster_whisper import WhisperModel
 
 model = WhisperModel("large-v2")
 
-
-# Define the prediction function
-def classify_text(text):
-    inputs = tokenizer(text, return_tensors="pt", truncation=True, padding=True)
-    outputs = model(**inputs)
-    logits = outputs.logits
-    predicted_class = torch.argmax(logits, dim=1).item()
-    predicted_names = labels_name[predicted_class]
-    return predicted_class,  predicted_names
+# Use a pipeline as a high-level helper
+from transformers import pipeline
+pipe = pipeline("text-classification", model="Peed911/french_sentiment_analysis")
 
 def transcribe_audio(audio):
   segments, info = model.transcribe(audio)
@@ -36,19 +22,19 @@ def transcribe_audio(audio):
     text = segment.text
   return text
 
-app = FastAPI()
+def sentiment_analyse_audio(audio):
+  """
+  input : audio files
 
-class Item(BaseModel):
-    text : str 
+  output : text
 
+  """
+  text = transcribe_audio(audio)
+  sentiment = pipe(text)
+  final_text = "En analysant l'audio d'entrée, le modèle Whisper d'openai transcrit le texte suivant : \n << %s >>, \n et prédit un sentiment << %s >> avec << %.2f >> pourcent de probabilité "%(text, sentiment[0]["label"], 100*sentiment[0]["score"])
+  return print(final_text)
 
-
-@app.post("/predict")
-def inference(file : UploadFile = File(...) ):
-    audio, _ = sf.read(file.file)
-    text = transcribe_audio(audio)
-    return {"text" : text}
 
 if __name__ == "__main__":
    audio_path = "common_voice_fr_17299384.wav"
-   print(transcribe_audio(audio_path))
+   print(sentiment_analyse_audio(audio_path))
